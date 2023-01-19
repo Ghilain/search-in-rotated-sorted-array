ddf5057c6dfde323e00926da10fc8655bfc0b81b
Ghilain <ishimweghilain5050@gmail.com>
2023-01-19 11:13:11 +0200

    add condition

--- challenge.js
+++ challenge.js
@@ -9,8 +9,20 @@ let hi = nums.length - 1;
 
 while (lo <= hi){
     let mid = lo+parseInt((hi-lo) / 2);
-    
+    if (nums[mid] === target){
+        return mid;
+    }
+
+    else if (nums[lo] <= nums[mid]){
+        // lo to mid is sorted
+
+    } 
+
+    else if (nums[mid] <= nums[hi]){
+        // mid to hi is sorted
+
+    } 
 
 }
-
+return -1;
 };
